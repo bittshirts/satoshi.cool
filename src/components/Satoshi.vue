497f0f8150f1d89ff1e329d05caef98ff8d33e6a
nminchow <noelminchow@gmail.com>
2020-11-24 22:22:57 -0500

    add alternate version

--- src/components/Satoshi.vue
+++ src/components/Satoshi.vue
@@ -6,11 +6,11 @@
     <!-- eslint-disable max-len -->
     <path
       id="path4"
-      d="m 8.0702 38.505 v -10.574 l -7.938 -6.609 v -10.575 l 7.938 -10.574 7.9371 10.574 v 10.575 l -4.0165 3.3045"
+      :d="path"
       fill="none"
       stroke="#020202"
       stroke-linejoin="round"
-      stroke-width="1.6"
+      :stroke-width="width"
     />
     <!-- eslint-enable max-len -->
     <use
@@ -19,6 +19,29 @@
     />
   </svg>
 </template>
+
+<script>
+import { useRoute } from 'vue-router';
+import { ref, watch } from 'vue';
+
+const defaultPath = 'm 8.0702 38.505 v -10.574 l -7.938 -6.609 v -10.575 l 7.938 -10.574 7.9371 10.574 v 10.575 l -4.0165 3.3045';
+
+export default {
+  setup() {
+    const route = useRoute();
+    const path = ref(defaultPath);
+    const width = ref(1.6);
+    const backward = ref(false);
+    if (route.query.v === '2') {
+      path.value = 'm 16.15 20.5 l -8 -8 l -8 8 l 8 8';
+      width.value = 0.5;
+      backward.value = true;
+    }
+    watch(() => route.query, () => window.location.reload());
+    return { path, width, backward };
+  },
+};
+</script>
 
 <style scoped>
 @keyframes dash {
